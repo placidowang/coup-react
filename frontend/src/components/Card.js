86ab5722fc5b6e0b9ddf3828098f71e0066d56f7
placidowang <placidow@gmail.com>
2020-08-15 04:00:17 -0500

    changed image location, now imported into components instead of being available in public folder since it was broken

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -1,6 +1,21 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import './Card.css'
+import ambassador from '../images/ambassador.png'
+import assassin from '../images/assassin.png'
+import captain from '../images/captain.png'
+import cardback from '../images/cardback.png'
+import contessa from '../images/contessa.png'
+import duke from '../images/duke.png'
+
+const cardImgData = {
+  ambassador: ambassador,
+  assassin: assassin,
+  captain: captain,
+  cardback: cardback,
+  contessa: contessa,
+  duke: duke,
+}
 
 class Card extends React.Component {
   render() {
@@ -12,7 +27,9 @@ class Card extends React.Component {
               <p className='x'>X</p>
               <div className={`card ${this.props.card.name.toLowerCase()} revealed`}>
                   <p className='card-name'>{this.props.card.name.toUpperCase()}</p>
-                  <img className='portrait' src={`./images/${this.props.card.name.toLowerCase()}.png`} alt={this.props.card.name} />
+                  {/* <img className='portrait' src={`./images/${this.props.card.name.toLowerCase()}.png`} alt={this.props.card.name} /> */}
+                  <img className='portrait' src={cardImgData[this.props.card.name.toLowerCase()]} alt={this.props.card.name} />
+
                   <div className='card-info'>
                     {/* <h3>{this.props.card.action}</h3> */}
                     {/* <h3>{this.props.card.effect}</h3> */}
@@ -24,8 +41,8 @@ class Card extends React.Component {
       } else {
         return(
           <div className={`card ${this.props.card.name.toLowerCase()}`}>
-              <p className='card-name'>{this.props.card.name.toUpperCase()}</p>
-            <img className='portrait' src={`./images/${this.props.card.name.toLowerCase()}.png`} alt={this.props.card.name} />
+            <p className='card-name'>{this.props.card.name.toUpperCase()}</p>
+            <img className='portrait' src={cardImgData[this.props.card.name.toLowerCase()]} alt={this.props.card.name} />
             <div className='card-info'>
               {/* <h3>{this.props.card.action}</h3> */}
               {/* <h3>{this.props.card.effect}</h3> */}
@@ -38,7 +55,7 @@ class Card extends React.Component {
     } else {
       return (
         <div className={'card'}>
-          <img className={'cardback'} src={'./images/cardback.png'} alt={'cardback'}/>
+          <img className={'cardback'} src={cardImgData['cardback']} alt={'cardback'}/>
         </div>
       )
     }
